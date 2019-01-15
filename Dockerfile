4393be5a8c656ba3db65b07842b933236190bf04
David Sn <divad.nnamtdeis@gmail.com>
2019-01-15 15:58:08 +0100

    Drop heirloom-mailx and use curl

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,7 @@ FROM alpine
 RUN apk --no-cache add \
         bash \
         curl \
-        openssl \
-        heirloom-mailx
+        openssl
 
 ADD upload.sh sendmail.sh /bin/
 RUN chmod +x /bin/*.sh
--- sendmail.sh
+++ sendmail.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
-# Send email via SMTP from bash using mailx
-# Example: ./sendmail.sh admin@localhost buildbot@localhost "Build status" "Build finished with result code 0" localhost:587 admin password
+# Send email via SMTP from bash using curl
+# Example: ./sendmail.sh "System admin" admin@localhost buildbot@localhost "Build status" "Build finished with result code 0" "smtps://localhost:465" admin password
 
-FROM=$1
-RECIPIENT=$2
-SUBJECT=$3
-MESSAGE=$4
-SERVER=$5
-USER=$6
-PASSWORD=$7
+SENDER=$1
+FROM=$2
+RECIPIENT=$3
+SUBJECT=$4
+MESSAGE=$5
+SERVER=$6
+USER=$7
+PASSWORD=$8
+
+# Generate message-id
+generate_id() {
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+}
+
+DOMAIN=$(echo $FROM | cut -d@ -f2)
+MESSAGE_ID="<$(generate_id 8).$(generate_id 16)@$DOMAIN>"
+
+# Get current timestamp
+DATE=$(date +"%a, %d %b %Y %H:%M:%S %z")
 
 # Output message to temporary file
 MAIL=$(mktemp -t mailXXXXXX)
-echo $MESSAGE > $MAIL
+cat << EOF > $MAIL
+Date: $DATE
+From: $SENDER <$FROM>
+To: $TO
+Subject: $SUBJECT
+Message-ID: $MESSAGE_ID
+User-Agent: drone-ci/sendmail.sh
+MIME-Version: 1.0
+Content-Type: text/plain; charset="utf8"
+
+$MESSAGE
+EOF
 
 # Send the mail!
-mailx -v \
-    -r $FROM \
-    -s $SUBJECT \
-    -S smtp="$SERVER" \
-    -S smtp-use-starttls \
-    -S smtp-auth=login \
-    -S smtp-auth-user="$USER" \
-    -S smtp-auth-password="$PASSWORD" \
-    -S ssl-verify=ignore \
-    $RECIPIENT < $MAIL
+curl --url $SERVER --ssl-reqd --insecure \
+    --mail-from $FROM --mail-rcpt $TO \
+    --upload-file $MAIL --user "$USER:$PASSWORD"
